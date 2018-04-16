cd73c9f18f860dd56d02ecc2fc3dc3766088ced3
Mahmoud Youssef <mahmoud.yusof27@gmail.com>
2018-04-16 18:11:59 +0200

    modifing navbar links to adabt

--- app/main.js
+++ app/main.js
@@ -5,16 +5,16 @@ portfolio.config(['$routeProvider', "$locationProvider", function($routeProvider
     $locationProvider.html5Mode(true);
 
     $routeProvider
-    .when('/home', {
+    .when('/', {
         templateUrl: "views/home.html",
-        controller: "homeController"
+        controller: "homeController",
     })
     .when('/tools', {
         templateUrl: "views/tools.html",
         controller: "ToolsController"
     })
     .otherwise({
-        redirectTo: "/home"
+        redirectTo: "/"
     });
 }]);
 
@@ -36,19 +36,39 @@ portfolio.controller("ToolsController", ["$scope", "$http", function($scope, $ht
 }]);
 
 
-// document.addEventListener("scroll", function(){
-
-// });
-
 document.addEventListener("scroll", function(){
     var back = document.getElementById("back");
     var win_y = window.scrollY;
     var win_h = window.innerHeight;
     var content = document.getElementById("content");
-    var cont_h = content.style.height;
 
-    back.style.backgroundPositionY = String(win_y/4) + "px";
+    back.style.backgroundPositionY = String(win_y/5) + "px";
 
-    content.style.top = String((win_y/2.5) + ((win_h / 2) - 100)) + "px";
+    content.style.top = String((win_y/3) + ((win_h / 2) - 100)) + "px";
+});
+document.addEventListener("load", function(){
+
 });
 
+// document.addEventListener("load", function(){
+//     var content = document.getElementById("content");
+//     content.style.display = "block";
+//     console.log("loaded");
+// });
+
+$(document).ready(function(){
+    var content = $("#content");
+    content.show();
+    
+});
+
+function dis(clicked){
+    var items = document.getElementsByClassName("nav-link");
+    
+    for (var i=0; i<items.length; i++){
+        items[i].className = "nav-link";
+    }
+
+    clicked.className = "nav-link active";
+    console.log("dis");
+}
